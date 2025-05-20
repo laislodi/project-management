a4e3bcc0123c5d9ec4712caaaa134d250b166fb5
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2025-05-20 15:18:40 -0600

    Fix bug, add import

--- src/components/buttons/IconButton.jsx
+++ src/components/buttons/IconButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Button from "./Button.jsx";
 import OptionModal from "../modal/OptionModal.jsx"
 import { H2 } from "../text/Headers.jsx";
